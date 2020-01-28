c8e2c8d93fb3915e8b49393d27f7040ae763655a
Matthew-Burfield <m1.burfield@gmail.com>
2020-01-28 21:29:59 +1000

    Added routes for signup, login and signout

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 const Mutation = {
   async createJournal(parent, args, ctx, info) {
     // TODO: Check if they are logged in
@@ -5,12 +8,68 @@ const Mutation = {
     const journal = await ctx.db.mutation.createJournal(
       {
         data: {
-          ...args
+          ...args,
+          user: {
+            connect: {
+              id: ctx.response.userId
+            }
+          }
         }
       },
       info
     );
     return journal;
+  },
+
+  async signup(parent, args, ctx, info) {
+    args.email = args.email.toLowerCase();
+    const password = await bcrypt.hash(args.password, 11);
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ["USER"] }
+        }
+      },
+      info
+    );
+    // create JWT token
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // Send the token back to the user via a cookie in the response
+    ctx.response.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365 // Keep the user signed in for 1 year
+    });
+    return user;
+  },
+
+  login: async (parent, args, ctx, info) => {
+    const user = await ctx.db.query.user(
+      {
+        where: {
+          email: args.email.toLowerCase()
+        }
+      },
+      "{ id name email permissions password }"
+    );
+    if (!user || !bcrypt.compareSync(args.password, user.password)) {
+      throw Error("The email address or password is wrong.");
+    }
+    // create JWT token
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // Send the token back to the user via a cookie in the response
+    ctx.response.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365 // Keep the user signed in for 1 year
+    });
+    delete user.password;
+    return user;
+  },
+
+  signout: (parent, args, ctx, info) => {
+    ctx.response.clearCookie("token");
+    return { message: "Success" };
   }
 };
 
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,7 +1,33 @@
 const { forwardTo } = require("prisma-binding");
+const bcrypt = require("bcrypt");
 
 const Query = {
-  journals: forwardTo("db")
+  me: (parent, args, ctx, info) => {
+    const { userId } = ctx.response;
+    if (!userId) {
+      return null;
+    }
+    return ctx.db.query.user(
+      {
+        where: { id: userId }
+      },
+      info
+    );
+  },
+
+  journals: (parent, args, ctx, info) => {
+    const { userId } = ctx.response;
+    if (!userId) {
+      return [];
+    }
+    return ctx.db.query.journals({
+      where: {
+        user: {
+          id: userId
+        }
+      }
+    });
+  }
 };
 
 module.exports = Query;
